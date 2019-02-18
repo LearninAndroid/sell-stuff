752c998d77b445ecd43f0fc43537df32e9a024ba
mtotodev05 <mtotodev05@gmail.com>
2019-02-18 17:37:30 +0300

    Setup product adapter

--- app/src/main/java/dev/mtoto/ecommerce/ui/adapter/ProductAdapter.kt
+++ app/src/main/java/dev/mtoto/ecommerce/ui/adapter/ProductAdapter.kt
@@ -1,26 +1,33 @@
 package dev.mtoto.ecommerce.ui.adapter
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.squareup.picasso.Picasso
+import dev.mtoto.ecommerce.R
 import dev.mtoto.ecommerce.model.Product
 
 class ProductAdapter(private val products: ArrayList<Product>) : RecyclerView.Adapter<ProductAdapter.ViewHolder>() {
 
 
-
-    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ProductAdapter.ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductAdapter.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.product_row,parent,false)
+        return ViewHolder(view)
     }
 
     override fun getItemCount() = products.size
 
-    override fun onBindViewHolder(p0: ProductAdapter.ViewHolder, p1: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onBindViewHolder(holder: ProductAdapter.ViewHolder, position: Int) {
+        Picasso.get().load(products[position].photoUrl).into(holder.image)
+        holder.title.text = products[position].title
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+        val image: ImageView = itemView.findViewById(R.id.photo)
+        val title: TextView = itemView.findViewById(R.id.title)
 
     }
 }
--- app/src/main/java/dev/mtoto/ecommerce/ui/home/Home.kt
+++ app/src/main/java/dev/mtoto/ecommerce/ui/home/Home.kt
@@ -1,11 +1,11 @@
 package dev.mtoto.ecommerce.ui.home
-
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.LinearLayoutManager
 import dev.mtoto.ecommerce.R
+import dev.mtoto.ecommerce.model.Product
 import dev.mtoto.ecommerce.ui.adapter.ProductAdapter
-
 import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.content_home.*
 
@@ -16,9 +16,14 @@ class Home : AppCompatActivity() {
         setContentView(R.layout.activity_home)
         setSupportActionBar(toolbar)
 
+        val products = arrayListOf<Product>()
+
+        for(i in 0..100){
+            products.add(Product("Organic Juice","http://via.placeholder.com/350/ffff00/ff0000",18.99))
+        }
         recycler_view.apply {
-            layoutManager = LinearLayoutManager(this@Home)
-            adapter = ProductAdapter()
+            layoutManager = GridLayoutManager(this@Home,2)
+            adapter = ProductAdapter(products)
         }
     }
 
