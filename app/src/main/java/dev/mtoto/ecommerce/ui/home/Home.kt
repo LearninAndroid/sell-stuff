39935c5c4ff7d629da41c8da810761595559a3eb
mtotodev05 <mtotodev05@gmail.com>
2019-03-18 16:36:38 +0300

    Success in switching fragments

--- app/src/main/java/dev/mtoto/ecommerce/ui/home/Home.kt
+++ app/src/main/java/dev/mtoto/ecommerce/ui/home/Home.kt
@@ -1,16 +1,14 @@
 package dev.mtoto.ecommerce.ui.home
+
 import android.os.Bundle
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayoutManager
 import android.util.Log.d
 import android.view.MenuItem
 import dev.mtoto.ecommerce.R
-import dev.mtoto.ecommerce.model.Product
-import dev.mtoto.ecommerce.ui.adapter.ProductAdapter
+
 import dev.mtoto.ecommerce.ui.products.jeans.JeansFragment
 import kotlinx.android.synthetic.main.activity_home.*
-import kotlinx.android.synthetic.main.content_home.*
 import kotlinx.android.synthetic.main.main.*
 
 class Home : AppCompatActivity() {
@@ -20,19 +18,23 @@ class Home : AppCompatActivity() {
         setContentView(R.layout.activity_home)
         setSupportActionBar(toolbar)
 
-        supportFragmentManager.beginTransaction().replace(R.id.frameLayout,HomeFragment()).commit()
+        supportFragmentManager.beginTransaction().replace(R.id.frameLayout, HomeFragment()).commit()
 
         navigation_view.setNavigationItemSelectedListener {
             it.isChecked = true
             drawerLayout.closeDrawers()
-            when(it.itemId){   
-                R.id.action_shorts -> d("kamau","Shorts was pressed")
-                R.id.action_jeans ->{
-                    supportFragmentManager.beginTransaction().replace(R.id.frameLayout,JeansFragment())
+            when (it.itemId) {
+                R.id.action_home -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.frameLayout, HomeFragment())
+                        .commit()
+                }
+                R.id.action_shorts -> d("kamau", "Shorts was pressed")
+                R.id.action_jeans -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.frameLayout, JeansFragment())
                         .commit()
                 }
             }
-             true
+            true
         }
 
         supportActionBar?.apply {
@@ -40,14 +42,7 @@ class Home : AppCompatActivity() {
             setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp)
         }
 
-        val products = arrayListOf<Product>()
-        for(i in 0..100){
-            products.add(Product("Organic Juice $i","http://via.placeholder.com/350/dddddd/000000",18.99))
-        }
-        recycler_view.apply {
-            layoutManager = GridLayoutManager(this@Home,2)
-            adapter = ProductAdapter(products)
-        }
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
--- app/src/main/java/dev/mtoto/ecommerce/ui/home/HomeFragment.kt
+++ app/src/main/java/dev/mtoto/ecommerce/ui/home/HomeFragment.kt
@@ -2,13 +2,27 @@ package dev.mtoto.ecommerce.ui.home
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.GridLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import dev.mtoto.ecommerce.R
+import dev.mtoto.ecommerce.model.Product
+import dev.mtoto.ecommerce.ui.adapter.ProductAdapter
+import kotlinx.android.synthetic.main.fragment_home.view.*
 
 class HomeFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_home,container,false)
+
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        val products = arrayListOf<Product>()
+        for (i in 0..100) {
+            products.add(Product("Organic Juice $i", "http://via.placeholder.com/350/dddddd/000000", 18.99))
+        }
+        root.recycler_view.apply {
+            layoutManager = GridLayoutManager(activity, 2)
+            adapter = ProductAdapter(products)
+        }
+        return root
     }
 }
