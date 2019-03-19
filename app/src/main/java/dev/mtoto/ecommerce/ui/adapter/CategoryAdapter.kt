471ea0775e6f9872df1a0a4bee460a2dc859eefb
mtotodev05 <mtotodev05@gmail.com>
2019-03-19 14:24:58 +0300

    Setup adapter

--- app/src/main/java/dev/mtoto/ecommerce/ui/adapter/CategoryAdapter.kt
+++ app/src/main/java/dev/mtoto/ecommerce/ui/adapter/CategoryAdapter.kt
@@ -1,4 +1,23 @@
 package dev.mtoto.ecommerce.ui.adapter
 
-class CategoryAdapter {
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+class CategoryAdapter(categories: List<String>) : RecyclerView.Adapter<CategoryAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
+
+    }
 }
